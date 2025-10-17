8e6d779e07202df5f167e231435baa9f5e5d605c
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2025-10-17 21:32:38 +0200

    astro base correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ziarnoryzu.github.io/10x-project/",
-  base: "/10x-project/",
+  base: "/10x-project",
   output: "server",
   integrations: [react(), sitemap()],
   server: { port: 3000 },
