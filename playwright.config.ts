8ad048e35694bc20b2b097cc724966bcdddb4d58
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2025-11-14 19:01:39 +0100

    e2e: enhance webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,9 +47,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev:e2e",
     url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
     timeout: 120 * 1000,
   },
 });
